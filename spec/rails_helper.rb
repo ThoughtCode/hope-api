5e50439bff38e176a596fa018f208ed14682a427
henry2992 <henry2992@hotmail.com>
2018-03-19 00:05:38 -0500

    Fix Coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,8 @@
 require 'simplecov'
-SimpleCov.start 'rails'
+SimpleCov.start 'rails' do
+  add_filter "app/admin/*"
+  add_filter "app/controllers/api/v1/api_controller.rb"
+end
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
