387e3194033055fc01b11aaef92bf772c8ddf474
Rai Romero <Rainieromadrid@gmail.com>
2018-04-09 09:18:29 -0500

    Fix testing facebook provider
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- spec/controllers/api/v1/customers/providers_controller_spec.rb
+++ spec/controllers/api/v1/customers/providers_controller_spec.rb
@@ -3,18 +3,40 @@ require 'rails_helper'
 RSpec.describe Api::V1::Customers::ProvidersController, type: :controller do
   before(:each) do
     @request.env['devise.mapping'] = Devise.mappings[:customer]
+    @test_users = Koala::Facebook::TestUsers.new(
+      app_id: Rails.application.secrets.facebook_api_id,
+      secret: Rails.application.secrets.facebook_secret
+    )
   end
 
   describe 'POST #facebook' do
     it 'return 200 and register the user' do
-      test_user = Koala::Facebook::TestUsers.new(
-        app_id: Rails.application.secrets.facebook_api_id,
-        secret: Rails.application.secrets.facebook_secret
-      )
+      user = @test_users.create(true, 'email')
       post :facebook, params: { customer: {
-        facebook_access_token: test_user.app_access_token
+        facebook_access_token: user['access_token']
       } }
       expect(response.status).to eq(200)
+    end
+    it 'return 200 and login the user already register' do
+      user = @test_users.create(true, 'email')
+      post :facebook, params: { customer: {
+        facebook_access_token: user['access_token']
+      } }
+      post :facebook, params: { customer: {
+        facebook_access_token: user['access_token']
+      } }
+      expect(response.status).to eq(200)
+    end
+    it 'return 422 unprocesable entity' do
+      user = @test_users.create(true)
+      post :facebook, params: { customer: {
+        facebook_access_token: user['access_token']
+      } }
+      expect(response.status).to eq(422)
+    end
+    it 'return 404 if token is missing' do
+      post :facebook
+      expect(response.status).to eq(400)
     end
   end
 end
