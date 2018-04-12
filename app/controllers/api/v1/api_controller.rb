03d8705cc2f246e5beaf5cc1b29f7f3c9233147c
Johnmer Bencomo <bjohnmer@gmail.com>
2018-04-12 11:31:29 -0500

    Refactor set_response method

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -78,9 +78,12 @@ module Api::V1
     end
 
     def set_response(status, message, data = [])
-      json = data.as_json
+      json = data.as_json.any? ? data.as_json['data'] : data
       data_name = 'data'
-      data_name = (json['data']['type'] if json['data'].any?) if json.any?
+      resource_name = if json.any?
+                        json.is_a?(Array) ? json.first['type'] : json['type']
+                      end
+      data_name = resource_name if json.any?
       render status: status, json: {
         message: message,
         data_name => data
