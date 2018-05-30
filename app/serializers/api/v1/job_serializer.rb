c867801c9c4224c716e7757da5c97c873ff71064
Rai Romero <Rainieromadrid@gmail.com>
2018-05-30 09:12:34 -0500

    fixing job serializer
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/serializers/api/v1/job_serializer.rb
+++ app/serializers/api/v1/job_serializer.rb
@@ -1,8 +1,8 @@
 class Api::V1::JobSerializer
   include FastJsonapi::ObjectSerializer
   set_id :hashed_id # optional
-  attributes :id, :property_id, :started_at, :finished_at, :duration, :total,
-             :status
+  attributes :property_id, :started_at, :finished_at, :duration, :total,
+             :status, :property, :agent
 
   attribute :job_details do |j|
     j.job_details.as_json(except: [:job_id], include: [:service])
