9867b10380d115888b530101bb0a72751b08f828
Rai Romero <Rainieromadrid@gmail.com>
2018-06-05 14:30:24 -0500

    Fixing active admin for properties
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/admin/property.rb
+++ app/admin/property.rb
@@ -1,4 +1,20 @@
 ActiveAdmin.register Property do
   permit_params :customer_id, :name, :neightborhood_id, :p_street, :number, :s_street, :details,
-                :cell_phone
+                :cell_phone, :additional_reference, :phone
+
+  form do |f|
+    f.inputs do
+      f.input :customer_id, :label => 'Cliente', as: :select, :collection => Customer.all.map{|u| ["#{u.first_name} #{u.last_name}", u.id]}
+      f.input :neightborhood_id, label: 'Barrio', as: :select, collection: Neightborhood.all.map{|b| ["#{b.name}", b.id]}
+      f.input :name
+      f.input :p_street
+      f.input :number
+      f.input :s_street
+      f.input :details
+      f.input :additional_reference
+      f.input :phone
+      f.input :cell_phone
+      f.actions
+    end
+  end
 end
