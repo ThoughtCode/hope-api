e237e02d8bed86d4e5ecf75c241906228bd34abc
Rai Romero <Rainieromadrid@gmail.com>
2018-06-06 12:24:38 -0500

    Fixing customer serializer
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/serializers/api/v1/customer_serializer.rb
+++ app/serializers/api/v1/customer_serializer.rb
@@ -2,5 +2,10 @@ class Api::V1::CustomerSerializer
   include FastJsonapi::ObjectSerializer
   set_type :customer # optional
   set_id :id # optional
+<<<<<<< Updated upstream
   attributes :first_name, :last_name, :email, :access_token, :avatar
+=======
+  attributes :first_name, :last_name, :email, :access_token, :avatar,
+             :national_id, :cell_phone
+>>>>>>> Stashed changes
 end
