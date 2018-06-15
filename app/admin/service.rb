7c22621953084d43bb810d8c744b868d07cda262
Rai Romero <Rainieromadrid@gmail.com>
2018-06-15 09:17:24 -0500

    Adding icon to service
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/admin/service.rb
+++ app/admin/service.rb
@@ -1,3 +1,4 @@
 ActiveAdmin.register Service do
-    permit_params :service_type_id, :type_service, :name, :quantity, :time, :price
+  permit_params :service_type_id, :type_service, :name, :quantity, :time, :price,
+                :icon
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180613173135) do
+ActiveRecord::Schema.define(version: 20180614150448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -148,6 +148,7 @@ ActiveRecord::Schema.define(version: 20180613173135) do
     t.boolean "quantity"
     t.float "time"
     t.float "price"
+    t.string "icon"
   end
 
 end
