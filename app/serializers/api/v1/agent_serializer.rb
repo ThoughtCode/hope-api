5c85becfad2bb006fa1b935dcef50e02971d43b8
Rai Romero <Rainieromadrid@gmail.com>
2018-06-22 12:13:30 -0500

    Fixing agent profile
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/serializers/api/v1/agent_serializer.rb
+++ app/serializers/api/v1/agent_serializer.rb
@@ -2,5 +2,6 @@ class Api::V1::AgentSerializer
   include FastJsonapi::ObjectSerializer
   set_type :agent # optional
   set_id :id # optional
-  attributes :first_name, :last_name, :email, :access_token, :avatar
+  attributes :first_name, :last_name, :email, :access_token, :avatar,
+             :national_id, :cell_phone
 end
