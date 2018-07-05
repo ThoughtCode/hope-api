37938cc907636d4c179b8d08643c7a17b667c4bc
Rai Romero <Rainieromadrid@gmail.com>
2018-07-05 15:15:02 -0500

    Removing details and cell phone from properties
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/admin/property.rb
+++ app/admin/property.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Property do
-  permit_params :customer_id, :name, :neightborhood_id, :p_street, :number, :s_street, :details,
-                :cell_phone, :additional_reference, :phone
+  permit_params :customer_id, :name, :neightborhood_id, :p_street, :number, :s_street,
+                :additional_reference, :phone
 
   form do |f|
     f.inputs do
@@ -10,10 +10,8 @@ ActiveAdmin.register Property do
       f.input :p_street
       f.input :number
       f.input :s_street
-      f.input :details
       f.input :additional_reference
       f.input :phone
-      f.input :cell_phone
       f.actions
     end
   end
--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,6 @@ class Property < ApplicationRecord
   include Hashable
   belongs_to :customer
   belongs_to :neightborhood
-  validates_presence_of :name, :neightborhood_id, :p_street, :number, :s_street,
-                        :details, :cell_phone
+  validates_presence_of :name, :neightborhood_id, :p_street, :number, :s_street
   has_many :jobs
 end
