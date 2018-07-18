e37340d788eb30a5e868c52cd8787cc7c11fa937
Rai Romero <Rainieromadrid@gmail.com>
2018-07-18 10:14:37 -0500

    Fixing minor
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/models/job.rb
+++ app/models/job.rb
@@ -66,7 +66,7 @@ class Job < ApplicationRecord
   end
 
   def can_review?(user)
-    reviews.where(owner: user).blank? && Time.now > started_at
+    reviews.where(owner: user).blank? && Time.now > started_at && (user == agent || user == property.customer)
   end
 
   private
