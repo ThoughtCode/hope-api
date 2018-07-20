9965a722705d7b02b92efe8f556dde54eef2aad1
Rai Romero <Rainieromadrid@gmail.com>
2018-07-20 12:32:19 -0500

    Fixing minor
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/serializers/api/v1/proposal_serializer.rb
+++ app/serializers/api/v1/proposal_serializer.rb
@@ -1,20 +1,20 @@
 class Api::V1::ProposalSerializer
   include FastJsonapi::ObjectSerializer
   set_id :hashed_id
-  attributes :id, :job, :agent
+  attributes :id, :job, :agent, :hashed_id
 
   attribute :agent_rewiews_count do |j|
-    j.agent.my_qualifications.count unless j.agent.nil?
+    j.agent&.my_qualifications&.count
   end
-  
+
   attribute :agent_rewiews_average do |j|
-    j.agent.reviews_average unless j.agent.nil?
+    j.agent&.reviews_average
   end
 
   attribute :agent_rewiews do |j|
-    j.agent.reviews unless j.agent.nil?
+    j.agent&.reviews
   end
-  
+
   attribute :agent do |j|
     Api::V1::AgentSerializer.new(j.agent) unless j.agent.nil?
   end
