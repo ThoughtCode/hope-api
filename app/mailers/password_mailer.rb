2a710163717f11fabaa1cd0d400b89efed955132
Rai Romero <Rainieromadrid@gmail.com>
2018-07-23 09:28:10 -0500

    Fixing minor
    Signed-off-by: Rai Romero <Rainieromadrid@gmail.com>

--- app/mailers/password_mailer.rb
+++ app/mailers/password_mailer.rb
@@ -2,7 +2,6 @@ class PasswordMailer < ApplicationMailer
   default from: 'info@nocnoc.com.ec'
 
   def reset_password_instructions(user, token, _params)
-    byebug
     @user = user
     @url = ENV['FRONTEND_URL'] + '/resetear/' + token if user.class.name == 'Customer'
     @url = ENV['FRONTEND_URL'] + '/agente/resetear/' + token if user.class.name == 'Agent'
