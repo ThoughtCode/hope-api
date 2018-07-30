18c0ea2729a2a28cafae47049e9f46db2b5579a3
José Castellanos <neuralboot19@gmail.com>
2018-07-30 17:12:53 -0500

    Payments Web hook

---
+++ app/controllers/api/v1/customers/payments_controller.rb
@@ -0,0 +1,18 @@
+module Api::V1::Customers
+  class PaymentsController < ApiController
+    skip_before_action :disable_access_by_tk
+    
+    def received
+      Rails.logger.info(request.raw_post)
+      set_response(
+        200, 'OK'
+      )
+    end
+    def update
+      Rails.logger.info(request.raw_post)
+      set_response(
+        200, 'OK'
+      )
+    end
+  end
+end
