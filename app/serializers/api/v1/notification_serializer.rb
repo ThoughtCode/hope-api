08c2a19192f5d988a8f2da911a663281205c5cdd
José Castellanos <neuralboot19@gmail.com>
2018-08-09 14:36:38 -0500

    Notification Serializer

--- app/serializers/api/v1/notification_serializer.rb
+++ app/serializers/api/v1/notification_serializer.rb
@@ -1,4 +1,8 @@
 class Api::V1::NotificationSerializer
   include FastJsonapi::ObjectSerializer
   attributes :text, :customer_id, :status, :agent_id
+
+  attribute :job do |j|
+    Api::V1::JobSerializer.new(j)
+  end
 end
