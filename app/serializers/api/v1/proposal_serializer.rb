9e93f817f7d382e4c1c9de5cf6d36b88b67bd116
José Castellanos <neuralboot19@gmail.com>
2018-09-12 13:06:03 -0500

    Fix Serializer Reviews

--- app/serializers/api/v1/proposal_serializer.rb
+++ app/serializers/api/v1/proposal_serializer.rb
@@ -3,6 +3,7 @@ class Api::V1::ProposalSerializer
   set_id :hashed_id
   attributes :id, :job, :agent, :hashed_id
 
+
   # attribute :agent_rewiews_count do |j|
   #   j.agent&.my_qualifications&.count
   # end
@@ -14,7 +15,43 @@ class Api::V1::ProposalSerializer
 
 
   attribute :agent do |j|
+    # Api::V1::AgentSerializer.new(j.agent) unless j.agent.nil?
+ 
+
     unless j.agent.nil?
+
+    reviews = j.agent.reviews.map do |c|
+              { 
+                id: c.hashed_id,
+                type: 'review',
+                attributes: {
+                  id: c.id,
+                  comment: c.comment,
+                  qualification: c.qualification,
+                  owner: {
+                    data: {
+                      id: c.owner.id,
+                      type: 'customer',
+                      attributes: {
+                        first_name: c.owner.first_name,
+                        last_name: c.owner.last_name,
+                        email: c.owner.email,
+                        access_token: c.owner.access_token,
+                        avatar: {
+                          url: c.owner.avatar.url,
+                        },
+                        national_id: c.owner.national_id,
+                        cell_phone: c.owner.cell_phone,
+                        hashed_id: c.owner.hashed_id,
+                        rewiews_count: c.owner.my_qualifications.count,
+                        rewiews_average: c.owner&.reviews_average
+                      }
+                    }
+                  }
+                }
+              }
+    end
+
     {
         data: {
           id: j.agent.id,
@@ -32,10 +69,14 @@ class Api::V1::ProposalSerializer
             rewiews_count: j.agent.my_qualifications.count,
             rewiews_average: j.agent.reviews_average,
             jobs_count: j.agent.jobs.count,
-            status: j.agent.status
+            status: j.agent.status,
+            rewiews:    {
+              data: reviews
+            }
           }
         }
       }
     end
   end
 end
+
