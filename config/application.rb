e23864a31f8da022e111a5c83c60f93e96a17a1c
José Castellanos <neuralboot19@gmail.com>
2018-09-13 15:04:50 -0500

    Application Conf time zone

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module HopeApi
 
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.time_zone = 'Quito'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
