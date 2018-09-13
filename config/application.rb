7470904d0838527cb2c8d51a82672425de8d888b
José Castellanos <neuralboot19@gmail.com>
2018-09-13 15:21:51 -0500

    Application

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
 require "sprockets/railtie"
-require 'slack-notifier'
 # require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
