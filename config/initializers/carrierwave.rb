a25901830411869c53f83cc52b82ebbdfeee5738
henry2992 <henry2992@hotmail.com>
2018-09-19 08:12:26 -0500

    CArrierwave

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,7 +5,7 @@ CarrierWave.configure do |config|
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"],     # required
     :region                 => 'us-east-1'                        # optional, defaults to 'us-east-1'
   }
-  config.fog_directory  = 'noc-noc'               # required
+  config.fog_directory  = 'nocnoc-ec'               # required
   #config.fog_host       = 'https://assets.example.com'           # optional, defaults to nil
   #config.fog_public     = false                                  # optional, defaults to true
   config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
