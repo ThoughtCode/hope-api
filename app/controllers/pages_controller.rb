51d5762c1980844515546cf101d3374c82533d40
Jose Castellanos <neuralboot19@gmail.com>
2019-02-04 09:24:05 -0500

    Check if add card controller got params

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,5 @@ class PagesController < ApplicationController
   def home; end
 
   def add_card
-  	puts '\n\n\n\n\n\n\n\n\n\n\n\n'
-  	puts params[:id]
-  	puts '\n\n\n\n\n\n\n\n\n\n\n\n'
-  	puts params[:email]
-  	puts '\n\n\n\n\n\n\n\n\n\n\n\n'
   end
 end
