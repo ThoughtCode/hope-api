00a180e22d9f590ff5e4eba3f7d22c19e7831587
Jose Castellanos <neuralboot19@gmail.com>
2019-02-19 12:09:48 -0500

    Fix save created job

--- app/controllers/api/v1/customers/jobs_controller.rb
+++ app/controllers/api/v1/customers/jobs_controller.rb
@@ -45,6 +45,7 @@ module Api::V1::Customers
       job.job_details = job_details
       cc = CreditCard.find(params[:job][:credit_card_id])
       job.credit_card = cc
+      Rails.logger.info(job)
       if job.save
         # Create Invoice (Mandar para cuando se cobre)
         invoice = Invoice.create(customer: current_user, job: job, invoice_detail_id: params[:job][:invoice_detail_id])
