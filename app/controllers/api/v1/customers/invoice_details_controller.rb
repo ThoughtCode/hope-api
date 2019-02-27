b431c436fc6f6e4772e71b975e19dfe58c9d857a
nintF1link <jalagut8@gmail.com>
2019-02-27 17:16:49 -0500

    Added update action to edit invoices

--- app/controllers/api/v1/customers/invoice_details_controller.rb
+++ app/controllers/api/v1/customers/invoice_details_controller.rb
@@ -1,7 +1,8 @@
 module Api::V1::Customers
   class InvoiceDetailsController < CustomerUsersController
     include Serializable
-    before_action :set_invoice_details, only: %i[destroy]
+    before_action :set_invoice_details, only: %i[destroy update]
+    before_action :invoice_belongs_to_customer?, only: %i[destroy update]
 
     def index
       invoices = current_user.invoice_details.where(deleted: false)
@@ -39,17 +40,28 @@ module Api::V1::Customers
       end
     end
 
+    def update
+      if @invoice_details.update(invoice_details_params)
+        set_response(200, 'Detalles de facturacion actualizados exitosamente', serialize_invoices(@invoice_details))
+      else
+        set_response(422, @invoice_details.errors.messages.values.join(', '))
+      end
+    end
+
     private
 
-    def invoice_details_params
-      params
-        .require(:invoice_detail)
-        .permit(:email, :identification, :identification_type, :social_reason, :address, :telephone)
-    end
+      def invoice_details_params
+        params
+          .require(:invoice_detail)
+          .permit(:email, :identification, :identification_type, :social_reason, :address, :telephone)
+      end
 
-    def set_invoice_details
-      @invoice_details = InvoiceDetail.find_by(id: params[:id])
-    end
+      def set_invoice_details
+        @invoice_details = InvoiceDetail.find(params[:id])
+      end
 
+      def invoice_belongs_to_customer?
+        set_response(422, 'No se pudo actualizar la factura porque no te pertenece') if @invoice_details.customer_id != current_user.id
+      end
   end
 end
