6f73816454ece4ba05d18f0dd25f3fc5c7b6763e
Johnmer Bencomo <bjohnmer@gmail.com>
2019-06-13 15:07:12 -0500

    Small fix in Admin -> Reviews

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,4 +1,4 @@
 ActiveAdmin.register Review do
-  permit_params :job_id, :owner_type, :owner_id, :comment, :qualification
+  permit_params :job_id, :owner_type, :owner_id, :comment, :qualification, :reviewee_id, :reviewee_type
   actions :all, :except => :destroy
 end
