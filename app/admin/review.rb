dd9b928b4c6e675fdbdbd27c27daaad2d129b11d
Johnmer Bencomo <bjohnmer@gmail.com>
2019-06-13 15:18:12 -0500

    Small fix in Admin -> Reviews

--- app/admin/review.rb
+++ app/admin/review.rb
@@ -1,4 +1,32 @@
 ActiveAdmin.register Review do
-  permit_params :job_id, :owner_type, :owner_id, :comment, :qualification, :reviewee_id, :reviewee_type
   actions :all, :except => :destroy
+  permit_params :job_id, :owner_type, :owner_id, :comment, :qualification, :reviewee_id, :reviewee_type
+
+  index do
+    selectable_column
+    id_column
+    column :job_id
+    column :owner, as: 'Calificador'
+    column :reviewee, as: 'Calificado'
+    column :qualification
+    column :comment
+    column "" do |resource|
+      links = ''.html_safe
+      links += link_to 'View', resource_path(resource), class: "member_link show_link"
+      links += link_to 'Edit', edit_resource_path(resource), class: "member_link edit_link"
+      links
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :owner_id, label: 'Calificador ID'
+      f.input :owner_type, label: 'Calificador Tipo'
+      f.input :comment
+      f.input :qualification
+      f.input :reviewee_id, label: 'Calificado ID'
+      f.input :reviewee_type, label: 'Calificado Tipo'
+      f.actions
+    end
+  end
 end
