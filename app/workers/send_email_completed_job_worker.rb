960e289216b855159cccbcde9e25d51215dbe327
Johnmer Bencomo <bjohnmer@gmail.com>
2019-06-13 15:43:21 -0500

    Bug Fix url missing on completed jobs

--- app/workers/send_email_completed_job_worker.rb
+++ app/workers/send_email_completed_job_worker.rb
@@ -5,6 +5,6 @@ class SendEmailCompletedJobWorker
   	agent = Agent.find(agent_id)
   	job = Job.find(job_id)
     CustomerMailer.send_job_completed(job.property.customer, job.hashed_id, ENV['FRONTEND_URL']).deliver
-    AgentMailer.send_job_completed(agent, job.hashed_id, url).deliver
+    AgentMailer.send_job_completed(agent, job.hashed_id, ENV['FRONTEND_URL']).deliver
   end
 end
